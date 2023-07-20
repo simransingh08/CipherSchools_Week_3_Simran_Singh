6a6d58d57b142412244ef34e4a8a1f09172759e1
Simran Singh <107848987+simransingh08@users.noreply.github.com>
2023-07-20 12:04:32 +0530

    Create Dynamic.cpp

---
+++ Dynamic.cpp
@@ -0,0 +1,29 @@
+// Dynamic letcure
+//----------------------------------------
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Cal{
+public:
+T1 x;
+T2 y;
+Cal(T1 x,T2 y){
+this->x = x;
+this->y = y;
+}
+T1 sum(){
+return x+y;
+}
+T1 sub(){
+return x-y;
+}
+};
+
+int main()
+{
+    Cal<int,int>c1(2,5);
+    Cal<double,int>c2(2.25,5);
+
+    return 0;
+}
